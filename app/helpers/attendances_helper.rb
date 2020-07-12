696ea7e84850ad1723c718d1d1d7ac3f2a027e08
sodium0221 <tatsuya.n.22128@gmail.com>
2020-07-12 09:51:10 +0000

    Fix show-page1

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -4,9 +4,14 @@ module AttendancesHelper
     # 受け取ったAttendanceオブジェクトが当日と一致するか評価します。
     if Date.current == attendance.worked_on
       return '出勤' if attendance.started_at.nil?
-      return '退勤' if attendance.started_at.present? && attendance.finished_at.nil?
     end 
     false
+  end
+  
+  def quitting_state(attendance)
+    if Date.current == attendance.worked_on
+      return '退勤' if attendance.started_at.present? && attendance.finished_at.nil?
+    end 
   end
   
   # 出勤時間と退勤時間を受け取り、在社時間を計算して返します。
