b9fe182a51fe8814efeb746c3d2c613741438c74
sodium0221 <tatsuya.n.22128@gmail.com>
2021-08-07 06:21:37 +0000

    Fix_seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ User.create!(name: "管理者",
                superior: true)
 end
              
-60.times do |n|
+10.times do |n|
   name = Faker::Name.name
   email = "sample-#{n+1}@email.com"
   password = "password"
